47e6d52c36c098fc3a8bbf2d7fcae0bdc93ad5ce
vicjuhl <victorkjellerupjuhl@me.com>
2026-01-13 15:08:37 +0100

    config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,12 @@
+import math
+
+n = 100
+
 conf = {
     "epoch_T_schedule": [10, 100],
     "N": 10,
-    "n": 100,
-    "indegree": 12
+    "n": n,
+    "indegree": 16,
+    "beta": 1/2 * math.log(n),
+    "tau": 1
 }

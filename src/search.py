a8c136ad253d9890487886614c944d74e1e93a5e
vicjuhl <victorkjellerupjuhl@me.com>
2026-01-14 12:52:04 +0100

    Search modifications

--- src/search.py
+++ src/search.py
@@ -1,27 +1,36 @@
 import numpy as np
 import pandas as pd
 import torch
+from typing import Tuple, Literal
+
 from config import conf
+from scoring import score
+from actions import perform_legal_action
 
-def score(s: np.ndarray) -> float:
-    return 42
-
-def run_episode(n_vars: int, horizon: int) -> dict[str, np.ndarray | int]:
+def run_episode(X: torch.Tensor, horizon: int) -> dict[str, torch.Tensor | int]:
     print(f"\nRunning episode with T={horizon}")
-    s = np.zeros((n_vars, n_vars))
+    d = X.shape[1]
+    s = torch.zeros((d, d))
+    l0 = score(s, X, 0)
+    r = score(s, X, l0)
+    assert abs(r) < 0.0001
+
     for t in range(horizon):
         if t % (horizon // 10) == 0:
             print(f"t = {t}")
-    s[1,1] = 1
-    return {"state": s, "score": score(s)}
+        s, a = perform_legal_action(s)
+        r = r - score(s, X, l0)
+        # Store SARS TODO
+    return {"state": s, "score": score(s, X, l0)}
 
-def search(df: pd.DataFrame) -> np.ndarray:
-    n_vars = len(df.columns)
-    best = {"state": np.zeros((n_vars, n_vars)), "score": 0}
+def search(df: pd.DataFrame) -> torch.Tensor:
+    d = len(df.columns)
+    X = torch.tensor(df.values)
+    best = {"state": torch.zeros((d, d)), "score": 0}
     for T in conf["epoch_T_schedule"]:
-        best_epsd = run_episode(n_vars, T)
-        if best_epsd["score"] > best["score"]:
-            best = best_epsd.copy()
+        epsd_best = run_episode(X, T)
+        if epsd_best["score"] > best["score"]:
+            best = epsd_best.copy()
     print("\nBest state:")
     print(best["state"])
     print(f"with score {best["score"]}")

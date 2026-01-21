011ea37c992b68187ebb1eee9c52892c4c4f7b9f
vicjuhl <victorkjellerupjuhl@me.com>
2026-01-21 15:40:40 +0100

    Simple MLP model

--- src/rlcd/model.py
+++ src/rlcd/model.py
@@ -1,16 +1,31 @@
 import torch
+from torch import nn
 import torch.nn.functional as F
 
 class QNetwork(torch.nn.Module):
     def __init__(self, d: int):
         super().__init__()
         self.d = d
-        # Layer defs here
+        self.fc1 = nn.Linear(d*d, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, d*d*3)
 
-    def forward(self, x) -> torch.Tensor:
-        """Returns a Q-table of dimension (N, N, 3).
-        N = |V|, last dimension is 0 = remove, 1 = add, 2 = reverse
+    def forward(self, s: torch.Tensor) -> torch.Tensor:
+        """Returns batchsize Q-tables of dimension (bs, d, d, 3).
+        Last dimension is 0 = remove, 1 = add, 2 = reverse
         """
-        # apply layers here, such as s = F.relu(self.conv1(x))
-        return torch.rand((self.d, self.d, 3))
+        batch_pass = len(s.shape) == 4
+        start_dim = 0
+        if batch_pass: # batch pass
+            bs = s.shape[0]
+            start_dim = 1
+        s = s.flatten(start_dim=start_dim)
+        s = F.relu(self.fc1(s))
+        s = F.relu(self.fc2(s))
+        s = self.fc3(s)
+        if batch_pass:
+            return s.view(bs, self.d, self.d, 3)
+        else:
+            return s.view(self.d, self.d, 3)
+        
 

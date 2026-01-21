b2dea0c4e934ea1edcfa3a265a6ddd3e17f84616
vicjuhl <victorkjellerupjuhl@me.com>
2026-01-21 15:41:41 +0100

    replay buffer

--- src/rlcd/config.py
+++ src/rlcd/config.py
@@ -3,10 +3,14 @@ import math
 n = 1000
 
 conf = {
-    "epoch_T_schedule": [10, 20],
+    "epoch_T_schedule": [100, 200],
     "N": 10,
     "n": n,
     "indegree": 16,
     "beta": math.log(n) * 50,
-    "tau": 1
+    "tau": 1,
+    "Q_lr": 1e-3,
+    "gamma": 0.98,
+    "xi": 0.995,
+    "batch_size": 32
 }
---
+++ src/rlcd/replay.py
@@ -0,0 +1,21 @@
+import random
+from collections import namedtuple, deque
+
+Transition = namedtuple(
+    'Transition',
+    ('s', 'a', 'r', 's_next')
+)
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        transitions = random.sample(self.memory, batch_size)
+        return Transition(*zip(*transitions)) # batch-array of Transitions -> Transition of batch-arrays.
+
+    def __len__(self):
+        return len(self.memory)

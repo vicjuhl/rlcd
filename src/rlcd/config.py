bd1a5293c240930d482f5ef5a5a596ee3758b654
vicjuhl <victorkjellerupjuhl@me.com>
2026-01-27 14:48:03 +0100

    decent params

--- src/rlcd/config.py
+++ src/rlcd/config.py
@@ -1,8 +1,9 @@
 import torch
 
 n = 1000
-d = 4
+d = 5
 reward_scale = 1e-3
+T = d * 2
 
 # Device configuration
 if torch.cuda.is_available():
@@ -16,20 +17,20 @@ device="cpu" # runs faster on my machine
 conf = {
     "device": device,
     "k_experiments": 5,
-    "T": 3,
-    "num_episodes": 5,
+    "T": T,
+    "num_episodes": 150,
     "d": d,
     "n": n,
-    "indegree": 3,
+    "indegree": 5,
     "noise_scale": 1,
     "beta": .02 * d * n * reward_scale,
-    "tau_prime": 5,
-    "Q_lr": 1e-0,
+    "tau_prime": 4,
+    "Q_lr": 1e-2,
     "gamma": 0.98,
     "xi": 0.98,
     "batch_size": 32,
     "reward_scale": reward_scale,
-    "step_penalty": .002 * d * n * reward_scale
+    "step_penalty": .02 * d * n * reward_scale / T
 }
 
 for k, v in conf.items():
